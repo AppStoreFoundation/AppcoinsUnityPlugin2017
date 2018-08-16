0fd6173f2cc65aba2dd389adaedfb93057f90f66
Fábio Vital <fabio.vital@aptoide.com>
2018-08-16 13:01:34 +0100

    Fix: Editor dll bugs

--- Appcoins Unity/Assets/AppcoinsUnity/Tools/BashCommand.sh
+++ Appcoins Unity/Assets/AppcoinsUnity/Tools/BashCommand.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 osascript -e 'activate application "/Applications/Utilities/Terminal.app"'
-cd '/Users/aptoide/Desktop/Appcoins Unity'
-if [ "$('/Users/aptoide/Library/Android/sdk/platform-tools/adb' get-state)" == "device" ]
-then
-'/Users/aptoide/Library/Android/sdk/platform-tools/adb' shell am start -n 'com.Company.ProductName/.UnityPlayerActivity' 2>&1 2>'/Users/aptoide/Documents/GitHub/AppcoinsUnityPlugin2017/Appcoins Unity/Assets/AppcoinsUnity/Tools/ProcessError.out'
-else
-echo error: no usb device found > '/Users/aptoide/Documents/GitHub/AppcoinsUnityPlugin2017/Appcoins Unity/Assets/AppcoinsUnity/Tools/ProcessError.out'
-fi
+cd '/Users/aptoide/Desktop/builds/Appcoins Unity'
+'/Applications/Android Studio.app/Contents/gradle/gradle-4.4/bin/gradle' assembleDebug 2>&1 2>'/Users/aptoide/Documents/GitHub/AppcoinsUnityPlugin2017/Appcoins Unity/Assets/AppcoinsUnity/Tools/ProcessError.out'
 echo 'done' > '/Users/aptoide/Documents/GitHub/AppcoinsUnityPlugin2017/Appcoins Unity/Assets/AppcoinsUnity/Tools/ProcessCompleted.out'
 exit
