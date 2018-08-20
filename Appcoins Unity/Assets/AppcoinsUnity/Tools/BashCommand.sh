bdbf3723f38363a46dfde9d04736a2c77c0f8ff6
Fábio Vital <fabio.vital@aptoide.com>
2018-08-20 12:59:18 +0100

    dll updates

--- Appcoins Unity/Assets/AppcoinsUnity/Tools/BashCommand.sh
+++ Appcoins Unity/Assets/AppcoinsUnity/Tools/BashCommand.sh
@@ -3,7 +3,7 @@ osascript -e 'activate application "/Applications/Utilities/Terminal.app"'
 cd '/Users/aptoide/Desktop/builds/Appcoins Unity'
 if [ "$('/Users/aptoide/Library/Android/sdk/platform-tools/adb' get-state)" == "device" ]
 then
-'/Users/aptoide/Library/Android/sdk/platform-tools/adb' shell am start -n 'com.aptoide.appcoins/com.aptoide.appcoins.UnityPlayerActivity' 2>&1 2>'/Users/aptoide/Documents/GitHub/AppcoinsUnityPlugin2017/Appcoins Unity/Assets/AppcoinsUnity/Tools/ProcessError.out'
+'/Users/aptoide/Library/Android/sdk/platform-tools/adb' -d install -r './build/outputs/apk/debug/Appcoins Unity-debug.apk' 2>&1 2>'/Users/aptoide/Documents/GitHub/AppcoinsUnityPlugin2017/Appcoins Unity/Assets/AppcoinsUnity/Tools/ProcessError.out'
 else
 echo error: no usb device found > '/Users/aptoide/Documents/GitHub/AppcoinsUnityPlugin2017/Appcoins Unity/Assets/AppcoinsUnity/Tools/ProcessError.out'
 fi
